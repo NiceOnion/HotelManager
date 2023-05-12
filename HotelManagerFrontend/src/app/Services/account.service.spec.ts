6327781a51bc5810315030791aaf41c580ef577e
NiceOnion <74778819+NiceOnion@users.noreply.github.com>
2023-05-12 10:04:05 +0200

    Op hoop van zegen

--- HotelManagerFrontend/src/app/Services/account.service.spec.ts
+++ HotelManagerFrontend/src/app/Services/account.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import { AccountService } from './account.service';
+import {account} from "../DataObjects/Accounts";
 
 describe('AccountServiceService', () => {
   let service: AccountService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AccountService]
+    });
     service = TestBed.inject(AccountService);
+    httpMock = TestBed.inject(HttpTestingController)
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  })
+
+  it('should return a list of accounts', () => {
+    const mockAccounts = [
+      {id: 1, password: '1', role: '', username: 'Account 1'},
+      {id: 2, password: '2', role: '', username: 'Account 2'},
+      {id: 3, password: '3', role: '', username: 'Account 3'},
+    ];
+    service.getAllAccounts().subscribe(accounts => {
+      expect(accounts.length).toBe(3);
+      expect(accounts).toEqual(mockAccounts);
+    });
+
+    const request = httpMock.expectOne('${service.aseUrlService.getURL()}/Accounts/All');
+    expect(request.request.method).toBe('GET');
+    request.flush(mockAccounts);
   });
 });
