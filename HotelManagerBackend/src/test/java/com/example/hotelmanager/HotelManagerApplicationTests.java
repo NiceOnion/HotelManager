6e8a7035d26000352ba17f6ce7a57fa6af35c20e
NiceOnion <74778819+NiceOnion@users.noreply.github.com>
2023-05-25 17:45:21 +0200

    CI test (added a little unit test)

--- HotelManagerBackend/src/test/java/com/example/hotelmanager/HotelManagerApplicationTests.java
+++ HotelManagerBackend/src/test/java/com/example/hotelmanager/HotelManagerApplicationTests.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class HotelManagerApplicationTests {
 
 	@Test
-	public void CITest2(){
+	public void CITest3(){
 		String string = "Hello World!";
 		assertEquals("Hello World!", string);
 	}
