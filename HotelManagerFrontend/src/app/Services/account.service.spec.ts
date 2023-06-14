9d953d0776b74d37f3f56e8da1e1664fbb37b08b
NiceOnion <74778819+NiceOnion@users.noreply.github.com>
2023-06-14 20:01:02 +0200

    Help

--- HotelManagerFrontend/src/app/Services/account.service.spec.ts
+++ HotelManagerFrontend/src/app/Services/account.service.spec.ts
@@ -1,31 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AccountService } from './account.service';
 
-describe('AccountServiceService', () => {
+describe('AccountService', () => {
   let service: AccountService;
   let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [AccountService]
+      providers: [AccountService],
     });
     service = TestBed.inject(AccountService);
-    httpMock = TestBed.inject(HttpTestingController)
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
     httpMock.verify();
-  })
+  });
 
   it('should return a list of accounts', () => {
     const mockAccounts = [
-      {id: 1, password: '1', role: '', username: 'Account 1'},
-      {id: 2, password: '2', role: '', username: 'Account 2'},
-      {id: 3, password: '3', role: '', username: 'Account 3'},
+      { ID: 1, password: '1', role: '', username: 'Account 1' },
+      { ID: 2, password: '2', role: '', username: 'Account 2' },
+      { ID: 3, password: '3', role: '', username: 'Account 3' },
     ];
-    service.getAllAccounts().subscribe(accounts => {
+
+    service.getAllAccounts().subscribe((accounts) => {
       expect(accounts.length).toBe(3);
       expect(accounts).toEqual(mockAccounts);
     });
